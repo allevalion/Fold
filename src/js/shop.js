6dde8f1ae439e9758f2100d0d4a28cdc8b69437c
allevalion <llukyanov@outlook.com>
2025-07-24 00:01:19 -0400

    refactor(card): вынес scroll-анимацию из index.css + добавил на страницу магазина

--- src/js/shop.js
+++ src/js/shop.js
@@ -1,3 +1,26 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const revealElements = document.querySelectorAll('.card');
+
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('fade-in--active');
+          observer.unobserve(entry.target);
+        }
+      });
+    },
+    {
+      threshold: 0.1,
+    }
+  );
+
+  revealElements.forEach((el) => {
+    el.classList.add('fade-in');
+    observer.observe(el);
+  });
+});
+
 document.addEventListener('click', function (event) {
   document.querySelectorAll('.dropdown.open').forEach((drop) => {
     if (!drop.contains(event.target)) {
