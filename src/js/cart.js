888258f992286c0219a440dc891f0c998c3204a7
allevalion <llukyanov@outlook.com>
2025-08-04 02:24:06 -0400

    fix: исправлена ошибка деплоя из-за неверного пути

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,6 +1,6 @@
 import { showNotification } from './common/notifications.js';
 import { updateCartCount } from './utils/cartUtils.js';
-import { initFloatingButton } from './utils/floatingButton.js';
+import { initFloatingButton } from '/js/utils/floatingButton.js';
 
 updateCartCount();
 
